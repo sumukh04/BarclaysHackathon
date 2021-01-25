e5efe9cd58eb42e57bda7636e6ae0b015471f207
sumukhg <gawsh4@gmail.com>
2021-01-25 17:26:10 +0530

    Changes in the controller

--- ecommerce/src/main/java/com/book/ecommerce/controller/BooksController.java
+++ ecommerce/src/main/java/com/book/ecommerce/controller/BooksController.java
@@ -1,14 +1,9 @@
 package com.book.ecommerce.controller;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 
-import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.book.ecommerce.models.Books;
